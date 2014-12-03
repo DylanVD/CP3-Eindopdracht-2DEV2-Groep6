f71224b29024a4de98db3c7772e313e9d96051e6
Dylan Van Dingenen <vandingenendylan@gmail.com>
2014-12-03 11:17:08 +0100

    nieuw

--- view/items/newProject.php
+++ view/items/newProject.php
@@ -6,6 +6,7 @@
          <form id="newProjectForm" action="index.php?page=project" method="post">
             <label for="title">Titel project:</label>
             <input type="title" name="title" />
+            <p>Druk "ENTER" om te beginnen met brainstormen</p>
         </form>
 
 
